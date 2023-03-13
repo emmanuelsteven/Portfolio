8f69c101f78f6bfb6d770be0524f51acbb91b539
emmanuelsteven <write2stevemmy@gmail.com>
2023-03-13 20:19:06 +0100

    update Jscript

--- dom.js
+++ dom.js
@@ -1,24 +1,21 @@
-
-const navyul= document.querySelector('#navy-ul');
+const navyul = document.querySelector('#navy-ul');
 const vet = document.querySelector('.vet-parents');
 const menux = document.querySelector('.menu-x');
 const lists = document.querySelectorAll('.links-nav');
 
-function openLink() { 
-    navyul.setAttribute('style', 'visibility:visible');
-    menux.setAttribute('style', 'visibility:visible');
-
+function openLink() {
+  navyul.setAttribute('style', 'visibility:visible');
+  menux.setAttribute('style', 'visibility:visible');
 }
-vet.addEventListener('click', openLink)
+vet.addEventListener('click', openLink);
 
-function menubtn(){
-    navyul.setAttribute('style','visibility:hidden')
-    menux.setAttribute('style', 'visibility:hidden');
-
+function menubtn() {
+  navyul.setAttribute('style', 'visibility:hidden');
+  menux.setAttribute('style', 'visibility:hidden');
 }
-menux.addEventListener('click', menubtn)
+menux.addEventListener('click', menubtn);
 
 function listFunc() {
-    lists.forEach((list) => list.addEventListener('click', menubtn));
+  lists.forEach((list) => list.addEventListener('click', menubtn));
 }
 listFunc();
